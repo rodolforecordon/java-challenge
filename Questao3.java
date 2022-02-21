286ebad1769b91882660b11f7d0fcce5c0f93737
rodolforecordon <rodolforecordon@gmail.com>
2022-02-20 23:21:28 -0300

    minor changes

--- Questao3.java
+++ Questao3.java
@@ -4,27 +4,28 @@ import java.util.Scanner;
 public class Questao3 {
     // function to check repetition
     static boolean checkRepetition(String str1, String str2) {
-        // conferir se as strings sao anagramas
         if ( str1.length() != str2.length() ) {
             return false;
         }
-        // tranfroma em arrays
+
         char[] array1 = str1.toCharArray();
         char[] array2 = str2.toCharArray();
-        // ordena os arrays
+
         Arrays.sort(array1);
         Arrays.sort(array2);
-        // cria strings com os arrays para verificar se são iguais
+
         String strArray1 = new String(array1);
         String strArray2 = new String(array2);
+
         return strArray1.equals(strArray2);
     }
 
-    // funcao para criar um string que será convertido em array com as combinacoes
-    // de caracateres que serao comparados
+    // function to creat a string to be converted in an array with
+    // the characters combinations to be compared
     static String createComparisonString(String palavra) {
         String subStr = "";
         String strToArray = "";
+        
         for (int i = 0; i < palavra.length(); i++) {
             for (int j = i + 1; j <= palavra.length(); j++) {
                 subStr = palavra.substring(i, j);
@@ -33,6 +34,7 @@ public class Questao3 {
                 }
             }
         };
+        
         return strToArray;
     }
 
